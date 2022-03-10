45a78bb6e895f1074bb2c79e6434923cc007bd6f
pangchiu <65621310+pangchiu@users.noreply.github.com>
2022-03-10 10:47:28 +0700

    sửa lỗi biến

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
     
-    public void yeu(int a, int b, int c,int a, int b, int c,int a, int b, int c,int a, int b, int c,int a, int b, int c,int a, int b, int c,int a, int b, int c,int a, int b, int c,int a, int b, int c,int a, int b, int c,int a, int b, int c,)
+    public void yeu(int a, int b, int c,int d, int e, int f,int g, int h, int k,int u, int l, int v,int j, int u, int v)
     {
      for(int i = 0;i < 80; i ++ ) {
             System.out.println("em yêu anh");
