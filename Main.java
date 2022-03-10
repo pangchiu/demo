661c9e531f5eb7238238e49e1dc973422e93d279
pangchiu <65621310+pangchiu@users.noreply.github.com>
2022-03-10 10:12:21 +0700

    upload main

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i = 0;i < 80; i ++ ) {
+            System.out.println("em yêu anh");
+
+        }
+
+
+        for(int i = 0;i < 80; i ++ ) {
+            System.out.println("anh yêu em");
+
+        }
+    }
+}
